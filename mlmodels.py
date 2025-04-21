1a0a1084099f234cd2a25736f3ebb916b3b12711
SJ Yogesh <102499551+sjyogesh23@users.noreply.github.com>
2025-04-21 17:59:16 +0530

    fixing deployment error 1.3.15

--- mlmodels.py
+++ mlmodels.py
@@ -14,7 +14,10 @@ def trainmodels(df, target_type, target):
     if target_type == "Numeric":
         with st.spinner("Initializing regression setup..."):
             start_time = time.time()
-            regression_setup(df, target=target, session_id=None)
+            regression_setup(df, target=target, session_id=1, fold=3, 
+    fold_shuffle=True,
+    html=False,
+    silent=True)
             st.write("✅ Setup completed in", round(time.time() - start_time, 2), "seconds")
         
         with st.spinner("Initializing regression setup pull..."):
